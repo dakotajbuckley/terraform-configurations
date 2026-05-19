1bafa604e53a1a63423dce60492deb1c287803c8
Dakota Buckley <statisticsman1@gmail.com>
2026-05-18 20:40:00 -0600

    Restructure files

--- subscriptions/dakotabuilds/main.tf
+++ subscriptions/dakotabuilds/main.tf
@@ -1,4 +1,4 @@
-data "azurerm_client_config" "current" {}
+# RESOURCE GROUPS
 
 resource "azurerm_resource_group" "dakotabuilds-rg" {
   name     = "dakotabuilds-rg"
@@ -10,6 +10,8 @@ resource "azurerm_resource_group" "dakotabuilds-denmark-rg" {
   location = "denmarkeast"
 }
 
+# STORAGE ACCOUNTS
+
 resource "azurerm_storage_account" "dakotabuilds" {
   name                     = "dakotabuilds"
   resource_group_name      = azurerm_resource_group.dakotabuilds-rg.name
@@ -18,16 +20,22 @@ resource "azurerm_storage_account" "dakotabuilds" {
   account_replication_type = "LRS"
 }
 
+# STORAEG CONTAINERS
+
 resource "azurerm_storage_container" "dakotabuild-terraform-state" {
   name                  = "dakotabuilds-terraform-state"
   storage_account_id    = azurerm_storage_account.dakotabuilds.id
   container_access_type = "private"
 }
 
+# DNS ZONES
+
 resource "azurerm_dns_zone" "dakotabuilds-dns-zone" {
   name                = "dakotabuilds.dev"
   resource_group_name = azurerm_resource_group.dakotabuilds-rg.name
 }
+
+# DNS ZONES
 
 resource "azurerm_dns_a_record" "test" {
   name                = "test"
@@ -36,6 +44,8 @@ resource "azurerm_dns_a_record" "test" {
   ttl                 = 300
   records             = ["76.149.229.188"]
 }
+
+# KEY VAULTS
 
 resource "azurerm_key_vault" "dakotabuilds-kv" {
   name                = "dakotabuilds-kv"
@@ -60,6 +70,8 @@ resource "azurerm_key_vault_access_policy" "terraform_current_user" {
     "Purge",
   ]
 }
+
+# KEY VAULT SECRETS
 
 resource "azurerm_key_vault_secret" "dakotabuilds-kv-first-name" {
   name         = "first-name"
@@ -91,6 +103,8 @@ resource "azurerm_key_vault_secret" "dakotabuilds-kv-favorite-food" {
   ]
 }
 
+# SERVICE PRINCIPALS
+
 resource "azuread_application" "k8s-external-secrets" {
   display_name = "k8s-external-secrets"
   owners       = [data.azurerm_client_config.current.object_id]
@@ -106,11 +120,7 @@ resource "azuread_service_principal_password" "k8s-external-secrets-password" {
     service_principal_id = azuread_service_principal.k8s-external-secrets.id
 }
 
-resource "azurerm_role_assignment" "external-secrets-kv" {
-    scope = azurerm_key_vault.dakotabuilds-kv.id
-    role_definition_name = "Key Vault Secrets User"
-    principal_id = azuread_application.k8s-external-secrets.client_id
-}
+# MODULES
 
 # Creates a VNet then will create as many subnets as you pass in then associate those with the vnet
 module "azure_virtual_network" {
@@ -124,65 +134,3 @@ module "azure_virtual_network" {
   }
 }
 
-# Creates an NSG with what ever security rules you specify. Then will assocate that NSG with as many subnets as you'd like through IDs
-module "azure_network_security_group" {
-  source                      = "../../modules/azure_network_security_group"
-  network_security_group_name = "dakotabuilds-nsg-01"
-  location                    = azurerm_resource_group.dakotabuilds-rg.location
-  resource_group_name         = azurerm_resource_group.dakotabuilds-rg.name
-  subnet_ids_to_associate     = [for key, value in module.azure_virtual_network.subnet_ids : value]
-  security_rules = {
-    "allow-web-traffic" = {
-      priority                   = 100
-      direction                  = "Inbound"
-      access                     = "Allow"
-      protocol                   = "Tcp"
-      source_port_range          = "*"
-      destination_port_ranges    = ["80", "443", "22"]
-      source_address_prefixes    = ["76.149.229.188/32"]
-      destination_address_prefix = "*"
-    }
-  }
-}
-
-module "azure_virtual_network_denmark" {
-  source              = "../../modules/azure_virtual_network"
-  resource_group_name = azurerm_resource_group.dakotabuilds-denmark-rg.name
-  location            = azurerm_resource_group.dakotabuilds-denmark-rg.location
-  vnet_name           = "dakotabuilds-denmark-vnet"
-  vnet_address_space  = ["10.10.0.0/16"]
-  subnets = {
-    "dakotabuilds-denmark-snet-01" = ["10.10.10.0/24"]
-  }
-}
-
-module "azure_network_security_group_denmark" {
-  source                      = "../../modules/azure_network_security_group"
-  network_security_group_name = "dakotabuilds-denmark-nsg-01"
-  location                    = azurerm_resource_group.dakotabuilds-denmark-rg.location
-  resource_group_name         = azurerm_resource_group.dakotabuilds-denmark-rg.name
-  subnet_ids_to_associate     = [for key, value in module.azure_virtual_network_denmark.subnet_ids : value]
-  security_rules = {
-    "allow-web-traffic" = {
-      priority                   = 100
-      direction                  = "Inbound"
-      access                     = "Allow"
-      protocol                   = "Tcp"
-      source_port_range          = "*"
-      destination_port_ranges    = ["80", "443", "22"]
-      source_address_prefixes    = ["76.149.229.188/32"]
-      destination_address_prefix = "*"
-    }
-  }
-}
-
-# module "azure_linux_virtual_machine_denmark" {
-#     source = "../../modules/azure_linux_virtual_machine"
-#     resource_group_name = azurerm_resource_group.dakotabuilds-denmark-rg.name
-#     location = "denmarkeast"
-#     nic_name = "dakotabuilds-nic"
-#     public_ip_name = "dakotabuilds-pip1"
-#     admin_username = "dakotab"
-#     subnet_id = module.azure_virtual_network_denmark.subnet_ids.dakotabuilds-denmark-snet-01
-#     virtual_machine_name = "dakotabuilds-vm"
-# }
